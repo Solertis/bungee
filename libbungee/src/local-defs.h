a2c5f812a5ab412df58c54553dc04fc21a14782e
Anand Babu (AB) Periasamy <ab@unlocksmith.org>
2012-08-03 01:08:35 -0700

    shell cmd framework and load/eval cmds

---
+++ libbungee/src/local-defs.h
@@ -0,0 +1,57 @@
+/*
+local-defs.c: non-public common definitions
+
+This file is part of Bungee.
+
+Copyright 2012 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef _LOCAL_DEFS_H
+#define _LOCAL_DEFSH
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+#include "config.h"
+#endif
+
+/* NLS Macros */
+#if HAVE_LOCALE_H
+# include <locale.h>
+#endif
+#if !HAVE_SETLOCALE
+# define setlocale(Category, Locale) /* empty */
+#endif
+
+#if ENABLE_NLS
+# include <libintl.h>
+# define _(Text) gettext (Text)
+#else
+# undef bindtextdomain
+# define bindtextdomain(Domain, Directory) /* empty */
+# undef textdomain
+# define textdomain(Domain) /* empty */
+# define _(Text) Text
+#endif
+#define N_(Text) Text
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LOCAL_DEFS_H */
