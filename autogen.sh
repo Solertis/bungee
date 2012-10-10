83e6300aa0a4eca5b902eed2c3d594c3172f7fbf
Harshavardhana <fharshav@redhat.com>
2012-10-10 09:51:06 +0000

    Add check for 'aclocal' to be installed before ./autogen.sh is executed

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-## Exit on error.
-set -e
 
 ## Generate bungee build scripts
+aclocal=$(which aclocal)
+[ -z $aclocal ] && {
+    echo "Please install automake, autoconf, libtool from your relevant distros"
+    exit 127 # exit with proper bash $? for "Command not found"
+}
 aclocal
 autoheader
 autoconf
